b815688402bd385d8ab9be2039474d5869e4b89c
m-gunes <mistafagunes@gmail.com>
2026-01-05 23:25:17 +0100

    Prompt app using builder pattern with fluent pattern

--- src/main/java/org/csystem/app/Application.java
+++ src/main/java/org/csystem/app/Application.java
@@ -1,12 +1,11 @@
 package org.csystem.app;
 
-import com.karandev.io.util.console.Console;
-import org.csystem.app.nestedclass.BuilderPatternSample;
 import org.csystem.app.nestedclass.BuilderPatternWithFluentPatternSample;
+import org.csystem.app.prompt.PromptApp;
 
 class Application {
     public static void run(String[] args)
     {
-        BuilderPatternWithFluentPatternSample.run();
+        PromptApp.run();
     }
 }
---
+++ src/main/java/org/csystem/app/prompt/PromptApp.java
@@ -0,0 +1,20 @@
+package org.csystem.app.prompt;
+
+import com.karandev.io.util.console.Console;
+
+public class PromptApp {
+    public static void run()
+    {
+        var prompt = Prompt.Builder.builder()
+                .setTitle("Alert")
+                .setMessage("Save")
+                .setOptionMessage("Yes")
+                .setNegativeOption("No")
+                .setNeutralOption("Cancel")
+                .build();
+
+        var option = prompt.show();
+
+        Console.writeLine(option);
+    }
+}
