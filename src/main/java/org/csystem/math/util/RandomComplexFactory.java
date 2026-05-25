c4b7854fa586f166983a85dde273e4aee457a7a4
m-gunes <mistafagunes@gmail.com>
2026-05-25 16:03:23 +0200

    create math util

---
+++ src/main/java/org/csystem/math/util/RandomComplexFactory.java
@@ -0,0 +1,28 @@
+package org.csystem.math.util;
+
+import org.csystem.math.Complex;
+import java.util.random.RandomGenerator;
+
+public class RandomComplexFactory {
+    private final RandomGenerator m_randomGenerator;
+
+    public RandomComplexFactory(RandomGenerator randomGenerator)
+    {
+        m_randomGenerator = randomGenerator;
+    }
+
+    public Complex createRandom(double origin, double bound)
+    {
+        return new Complex(m_randomGenerator.nextDouble(origin, bound), m_randomGenerator.nextDouble(origin, bound));
+    }
+
+    public Complex [] createRandom(int count, double origin, double bound)
+    {
+        var result = new Complex[count];
+
+        for (var i = 0; i < count; i++)
+            result[i] = createRandom(origin, bound);
+
+        return result;
+    }
+}
