f45af85de0d0d05ba91846cc9691daac04d1e260
m-gunes <mistafagunes@gmail.com>
2026-07-24 10:12:04 -0700

    Stream examples 2

--- src/main/java/org/csystem/app/streams/StreamApp.java
+++ src/main/java/org/csystem/app/streams/StreamApp.java
@@ -2,9 +2,15 @@ package org.csystem.app.streams;
 
 import com.karandev.io.util.console.Console;
 import org.csystem.util.datasource.factory.ProductFactory;
+import org.csystem.util.datasource.factory.StaffFactory;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 
 import static com.karandev.io.util.console.CommandLineArgs.checkLengthEquals;
 
@@ -14,7 +20,11 @@ public class StreamApp {
     {
 //        getProductGraterThenGivenStock(args);
 //        getBetweenStock(args);
-        getBetweenCost(args);
+//        getBetweenCost(args);
+//        getBetweenDates(args);
+//        getBetweenYears(args);
+//        getStaffsOnLeave(args);
+        getStaffsOnLeaveWithShortcut(args);
     }
 
     private static void firstEx(String[] args)
@@ -118,4 +128,112 @@ public class StreamApp {
             Console.Error.writeLine("Error occurred :%s", e.getMessage());
         }
     }
+
+    // 4. Aşağıdaki demo örnekte komut satırından alınan minDate ve maxDate değerlerine göre (minDate, maxDate) aralığında doğan çalışanlar listelenmiştir
+    private static void getBetweenDates(String[] args)
+    {
+        try {
+            checkLengthEquals(args.length, 3, "Wrong number of arguments");
+            var staffs = StaffFactory.loadFromTextFile(args[0]).getStaffAsArray();
+            var formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            var minDate = LocalDate.parse(args[1], formatter);
+            var maxDate = LocalDate.parse(args[2], formatter);
+
+            Arrays.stream(staffs)
+                    .filter(s -> s.getBirthDate().isAfter(minDate))
+                    .filter(s -> s.getBirthDate().isBefore(maxDate))
+                    .forEach(Console::writeLine);
+        }
+        catch (DateTimeParseException ignore) {
+            Console.Error.writeLine("Invalid date format");
+        }
+        catch (IOException e) {
+            Console.Error.writeLine("IO Error occurred :%s", e.getMessage());
+        }
+        catch (Exception e) {
+            Console.Error.writeLine("Error occurred :%s", e.getMessage());
+        }
+
+    }
+
+    // 5. Aşağıdaki demo örnekte komut satırından alınan minYear ve maxYear değerlerine göre [minYear, maxYear] aralığında doğan çalışanlar listelenmiştir
+    private static void getBetweenYears(String[] args)
+    {
+        try {
+            checkLengthEquals(args.length, 3, "Wrong number of arguments");
+            var staffs = StaffFactory.loadFromTextFile(args[0]).getStaffAsArray();
+            var minYear = Integer.parseInt(args[1]);
+            var maxYear = Integer.parseInt(args[2]);
+
+            Arrays.stream(staffs)
+                    .filter(s -> minYear <= s.getBirthDate().getYear())
+                    .filter(s -> s.getBirthDate().getYear() <= maxYear)
+                    .forEach(Console::writeLine);
+        }
+        catch (NumberFormatException ignore) {
+            Console.Error.writeLine("Year values must be numeric");
+        }
+        catch (IOException e) {
+            Console.Error.writeLine("IO Error occurred :%s", e.getMessage());
+        }
+        catch (Exception e) {
+            Console.Error.writeLine("Error occurred :%s", e.getMessage());
+        }
+    }
+
+    // 6. Aşağıdaki demo örnekte komut satırından alınan SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY biçimindeki yazılardan biri şeklinde alınan haftanın günü bilgisine göre ilgili günde izni olan çalışanlar listelenmektedir
+    private static void getStaffsOnLeave(String [] args)
+    {
+
+        try {
+            checkLengthEquals(args.length, 2, "Wrong number of arguments");
+            var staffs = StaffFactory.loadFromTextFile(args[0]).getStaffAsArray();
+            var restWeekDay = DayOfWeek.valueOf(args[1].trim().toUpperCase());
+
+            Arrays.stream(staffs)
+                    .filter(s -> s.getRestDay() == restWeekDay)
+                    .forEach(Console::writeLine);
+        }
+        catch (IllegalArgumentException ignore) {
+            Console.Error.writeLine("Invalid weekday format");
+        }
+        catch (IOException e) {
+            Console.Error.writeLine("IO Error occurred :%s", e.getMessage());
+        }
+        catch (Exception e) {
+            Console.Error.writeLine("Error occurred :%s", e.getMessage());
+        }
+    }
+
+    // 7. Aşağıdaki demo örnekte komut satırından alınan SUN, MON, TUE, WED, THU, FRI, SAT biçimindeki yazılardan
+    // biri şeklinde alınan haftanın günü bilgisine göre ilgili günde izni olan çalışanlar listelenmektedir.
+    // Örnekte alınan değerlerin geçerliliği kontrol edilmektedir. Değerler yalnızca belirtildiği gibi alınabilmektedir.
+    private static void getStaffsOnLeaveWithShortcut(String [] args)
+    {
+        try {
+            checkLengthEquals(args.length, 2, "Wrong number of arguments");
+            var staffs = StaffFactory.loadFromTextFile(args[0]).getStaffAsArray();
+            var restWeekDay = args[1].trim().toUpperCase();
+
+            if (restWeekDay.length() != 3)
+                return;
+
+            Arrays.stream(staffs)
+                    .filter(s -> s.getRestDay().toString().startsWith(restWeekDay))
+                    .forEach(Console::writeLine);
+        }
+        catch (IllegalArgumentException ignore) {
+            Console.Error.writeLine("Invalid weekday format");
+        }
+        catch (IOException e) {
+            Console.Error.writeLine("IO Error occurred :%s", e.getMessage());
+        }
+        catch (Exception e) {
+            Console.Error.writeLine("Error occurred :%s", e.getMessage());
+        }
+
+    }
+
+
+
 }
